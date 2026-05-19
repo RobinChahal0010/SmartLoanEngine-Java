6339cf22f6aa16986a9d5bf338446faa99e4faea
Robinpreet Singh Chahal <mantar.1911@gmail.com>
2026-05-19 07:30:47 +0530

    Remove unnecessary comment before loan evaluation

--- main.java
+++ main.java
@@ -93,7 +93,7 @@ public class main {
         int score =
             CreditScoreCalculator.calculateScore(c);
 
-        // Evaluate Loan
+        
         LoanService.evaluateLoan(c, score);
 
         sc.close();
